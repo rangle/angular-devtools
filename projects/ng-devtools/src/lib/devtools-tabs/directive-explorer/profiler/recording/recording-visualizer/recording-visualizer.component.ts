8eb0d1ff135568fa94651f051c21ac92da9f0c67
Sumit Arora <er.sumitarora@gmail.com>
2020-03-14 21:29:09 -0400

    feat(devtools): adding piechart for profiler

--- projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/recording-visualizer/recording-visualizer.component.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/recording-visualizer/recording-visualizer.component.ts
@@ -11,6 +11,18 @@ export class RecordingVisualizerComponent {
   profilerBars: FlamegraphNode[] = [];
   selectedEntry: FlamegraphNode = null;
 
+  // graph options
+  gradient = false;
+  showLegend = true;
+  showLabels = false;
+  isDoughnut = false;
+  legendPosition = 'bottom';
+  graphData = [];
+  view: any[] = [200, 200];
+  colorScheme = {
+    domain: ['#E71D36', '#2EC4B6', '#FF9F1C', '#011627'],
+  };
+
   @Input() set bars(data: FlamegraphNode[]) {
     this.selectedEntry = null;
     this.profilerBars = data;
@@ -20,9 +32,31 @@ export class RecordingVisualizerComponent {
 
   selectFrame(frame: RawData): void {
     this.selectedEntry = frame as FlamegraphNode;
+    this.renderGraph(this.selectedEntry);
   }
 
   get availableWidth(): number {
     return this._el.nativeElement.querySelector('.level-profile-wrapper').offsetWidth;
+  }
+
+  formatPieChartData(flameGraphNode: FlamegraphNode): { name: string; value: number }[] {
+    const graphData: { name: string; value: number }[] = [];
+    flameGraphNode.original.directives.forEach(node => {
+      graphData.push({
+        name: node.name,
+        value: node.changeDetection,
+      });
+      Object.keys(node.lifecycle).forEach(key => {
+        graphData.push({
+          name: key,
+          value: node.lifecycle[key],
+        });
+      });
+    });
+    return graphData;
+  }
+
+  renderGraph(node: FlamegraphNode): void {
+    this.graphData = this.formatPieChartData(node);
   }
 }
--- projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/recording-visualizer/recording-visualizer.module.ts
+++ projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/profiler/recording/recording-visualizer/recording-visualizer.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RecordingVisualizerComponent } from './recording-visualizer.component';
 import { CommonModule } from '@angular/common';
 import { NgxFlamegraphModule } from 'ngx-flamegraph';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   declarations: [RecordingVisualizerComponent],
-  imports: [CommonModule, NgxFlamegraphModule],
+  imports: [CommonModule, NgxFlamegraphModule, NgxChartsModule],
   exports: [RecordingVisualizerComponent],
 })
 export class RecordingVisualizerModule {}
