4c2e8550a25f67fff0c47ea953d5f55a5911c370
AleksanderBodurri <alex.bodurri@mail.utoronto.ca>
2020-03-22 20:20:33 -0400

    refactor: type node properly; simplify logic in select-component.ts

--- projects/ng-devtools-backend/src/lib/client-event-subscribers.ts
+++ projects/ng-devtools-backend/src/lib/client-event-subscribers.ts
@@ -25,6 +25,7 @@ import { setConsoleReference } from './selected-component';
 import { unHighlight } from './highlighter';
 import { getAngularVersion, appIsAngularInDevMode, appIsSupportedAngularVersion } from './angular-check';
 import { observeDOM, getDirectiveId, getDirectiveForest, indexDirectiveForest } from './component-tree-identifiers';
+import { IndexedNode } from './observer/identity-tracker';
 
 export const subscribeToClientEvents = (messageBus: MessageBus<Events>): void => {
   messageBus.on('shutdown', shutdownCallback(messageBus));
@@ -90,9 +91,8 @@ const stopProfilingCallback = (messageBus: MessageBus<Events>) => () => {
 };
 
 const selectedComponentCallback = (position: ElementPosition) => {
-  // TODO: figure out proper typing of the node variable.
   const node = queryDirectiveForest(position, getDirectiveForest());
-  setConsoleReference(node);
+  setConsoleReference(node as IndexedNode);
 };
 
 const getNestedPropertiesCallback = (messageBus: MessageBus<Events>) => (
