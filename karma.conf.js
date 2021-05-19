a16feffdab73afe1b7987756f6b094e5e8408259
mgechev <mgechev@gmail.com>
2021-05-19 16:42:56 -0700

    build: revert karma config changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,13 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox'],
-      },
-    },
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
   });
